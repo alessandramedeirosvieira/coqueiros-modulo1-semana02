bf4d570b7eb07941dd364e4144b1e89009bc39ae
Alessandra Medeiros Vieira <alessandra_m_vieira@estudante.sesisenai.org.br>
2023-02-12 00:22:27 -0300

    add arquivos

---
+++ SLN-Semana02-POO/Semana02-POO/CentroDeCusto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana02_POO
+{
+    public class CentroDeCusto
+    {
+        public CentroDeCusto (int id,
+                               string descrição,
+                               DateTime dataCriação,
+                               decimal verbaLiberada,
+                               int anoVerbaLiberada);
+
+    }
+}
--- SLN-Semana02-POO/Semana02-POO/Program.cs
+++ SLN-Semana02-POO/Semana02-POO/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Semana02_POO;
+
 Console.WriteLine("Hello, World!");
+CentroDeCusto centroDeCusto = new CentroDeCusto(1, "Teste", DateTime.Now, 1500.0M, 2023);
+
+
